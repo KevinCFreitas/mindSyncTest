afe6ba00eb8992c6fd07c02b4d4025acee3b6264
Kevin_Freitas <Kevin2004clementefreitas@gmail.com>
2025-05-25 16:47:16 -0400

    feat: atualiza app.js com mensagens personalizadas e formulários

--- app.js
+++ app.js
@@ -11,7 +11,17 @@ let qrCodeDataURL = '';
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        headless: true,
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
